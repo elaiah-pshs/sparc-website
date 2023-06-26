6ff1992713f1bfe7a64454f231f68fdc47035ca4
Meds Pogi <69465320+MedsAsperin@users.noreply.github.com>
2023-06-26 13:46:05 +0800

    Reconfigure hover styles for navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -43,8 +43,8 @@
 
 <nav class="navbar">
     <ul class="navbar-items">
-        <li on:mouseover={toggleSidebar} on:focus={toggleSidebar} on:mouseout={hideSidebar} on:blur={hideSidebar} class="sidebar-toggler">
-            <img class="sidebar-toggler-icon" src="icons/sidebar.svg" alt="Open sidebar">
+        <li on:mouseover={toggleSidebar} on:focus={toggleSidebar} on:mouseout={hideSidebar} on:blur={hideSidebar} class="navbar-item">
+            <img class="navbar-link navbar-icon" src="icons/sidebar.svg" alt="Open sidebar">
         </li>
         <li class="navbar-path">
             {#if links.length <= 4}
