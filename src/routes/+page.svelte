313fb028d9064343e28b75cd26907d51499ea958
mathematicalcoder <ktachan314@gmail.com>
2023-06-29 22:22:28 +0800

    Add announcement template and minor update on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,30 @@
 <script>
-	import Topbar from '$lib/Topbar.svelte';
+    import { onMount } from 'svelte';
+    import '../styles/root.scss';
+
+    let display = "";
+
+    onMount(() => {
+        fetch("./templates/test.html")
+            .then((res) => res.text())
+            .then((text) => {
+                display = text;
+            })
+            .catch((e) => console.error(e));
+    });
 </script>
 
 <svelte:head>
-	<title>Home</title>
+    <title>Welcome to SPARC 2028!</title>
 </svelte:head>
 
-<Topbar />
+<!-- <main>
+    {@html display}
+</main> -->
 
-<main>
-	<h1>Home</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</main>
+<!--
+This script places a badge on your repl's full-browser view back to your repl's cover
+page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+teal, blue, blurple, magenta, pink!
+<script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+-->
--- src/routes/announcements/+page.svelte
+++ src/routes/announcements/+page.svelte
@@ -6,5 +6,19 @@
 
 <main>
     <h1>Announcements</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <div class="announcement">
+        <h2>Test Announcement</h2>
+        <h3><i>By SPARC '28</i></h3>
+        <p>Hello!</p>
+    </div>
+
+    <style>
+    .announcement {
+    background-color: #ac4c32;
+    color: white;
+    border-radius: 25px;
+    border: 2px solid white;
+    padding: 20px;
+    }
+    </style>
 </main>
