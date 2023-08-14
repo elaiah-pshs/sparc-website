22f8c18896ab41468bd915471b97e0f75746cb48
mathematicalcoder <ktachan314@gmail.com>
2023-08-14 19:59:57 +0800

    recreated announcements using Box.svelte

---
+++ src/lib/Box.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let header;
+    export let author;
+    export let body;
+</script>
+
+<div class="box">
+
+<h2>{header}</h2>
+<h3><i>By {author}</i></h3>
+<p>{body}</p>
+
+<style>
+    .box {
+        background-color: #ac4c32;
+        color: white;
+        border-radius: 25px;
+        border: 2px solid white; padding: 20px;
+    }
+</style>
+
+</div>
--- src/routes/announcements/+page.svelte
+++ src/routes/announcements/+page.svelte
@@ -1,22 +1,12 @@
+<script>
+    import Box from "$lib/Box.svelte";
+</script>
+
 <svelte:head>
 	<title>Announcements</title>
 </svelte:head>
 
 <main>
     <h1>Announcements</h1>
-    <div class="announcement">
-        <h2>Test Announcement</h2>
-        <h3><i>By SPARC '28</i></h3>
-        <p>Hello!</p>
-    </div>    
+    <Box header="Test Announcement" author="SPARC '28" body="Hello!" />
 </main>
-
-<style>
-    .announcement {
-        background-color: #ac4c32;
-        color: white;
-        border-radius: 25px;
-        border: 2px solid white;
-        padding: 20px;
-    }
-</style>
