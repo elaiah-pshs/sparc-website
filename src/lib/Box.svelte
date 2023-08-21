456138de50025a6c01b0236a8837bb41b622bb20
Meds Pogi <69465320+MedsAsperin@users.noreply.github.com>
2023-08-21 11:12:03 +0800

    Format Box.svelte

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -1,22 +1,21 @@
 <script>
-    export let header;
-    export let author;
-    export let body;
+	export let header;
+	export let author;
+	export let body;
 </script>
 
 <div class="box">
-
-<h2>{header}</h2>
-<h3><i>By {author}</i></h3>
-<p>{body}</p>
+	<h2>{header}</h2>
+	<h3><i>By {author}</i></h3>
+	<p>{body}</p>
+</div>
 
 <style>
-    .box {
-        background-color: #ac4c32;
-        color: white;
-        border-radius: 25px;
-        border: 2px solid white; padding: 20px;
-    }
+	.box {
+		background-color: #ac4c32;
+		color: white;
+		border-radius: 25px;
+		border: 2px solid white;
+		padding: 20px;
+	}
 </style>
-
-</div>
